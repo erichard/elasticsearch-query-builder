35598369150ee14199bf65c6daeab8413fc5fa87
Erwan Richard <erwan.richard@lephare.com>
2020-07-20 15:33:30 +0200

    Rewrite geodistance query

--- src/Query/GeoDistanceQuery.php
+++ src/Query/GeoDistanceQuery.php
@@ -4,25 +4,37 @@ namespace Erichard\ElasticQueryBuilder\Query;
 
 class GeoDistanceQuery implements QueryInterface
 {
+    /** @var string */
     private $distance;
-    private $pinLocation;
+
+    /** @var array|float[]|int[] */
+    private $position;
+
+    /** @var string */
     private $field;
 
-    public function setDistance($distance)
+    public function __construct(string $distance, string $field, array $position)
+    {
+        $this->distance = $distance;
+        $this->field = $field;
+        $this->position = $position;
+    }
+
+    public function setDistance(string $distance): self
     {
         $this->distance = $distance;
 
         return $this;
     }
 
-    public function setPinLocation($pinLocation)
+    public function setPosition(array $position): self
     {
-        $this->pinLocation = $pinLocation;
+        $this->position = $position;
 
         return $this;
     }
 
-    public function setField($field)
+    public function setField(string $field)
     {
         $this->field = $field;
 
@@ -31,10 +43,25 @@ class GeoDistanceQuery implements QueryInterface
 
     public function build(): array
     {
+        if (empty($this->distance)) {
+            throw new QueryException('You should call GeoDistanceQuery::setDistance() before building this query');
+        }
+
+        if (empty($this->field)) {
+            throw new QueryException('You should call GeoDistanceQuery::setField() before building this query');
+        }
+
+        if (empty($this->position)) {
+            throw new QueryException('You should call GeoDistanceQuery::setPosition() before building this query');
+        }
+
         return [
             'geo_distance' => [
                 'distance' => $this->distance,
-                $this->field ? $this->field : 'geolocation' => $this->pinLocation,
+                $this->field => [
+                    'lat' => $this->position[0],
+                    'lon' => $this->position[1],
+                ],
             ],
         ];
     }
